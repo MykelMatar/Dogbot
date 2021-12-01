48db41e8032150830de35eb35f84a4227836daea
mykelmatar <Michaelfm01@gmail.com>
2021-12-01 12:16:41 -0800

    updated embed to include modpack and server status

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const {Client, MessageEmbed, Message} = require('discord.js');
+const {Client, MessageEmbed, Message} = require('discord.js-12');
 const config = require('./config.json');
 const util = require('minecraft-server-util')
 const client = new Client() //({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });   // Discord.js 13 requires user to specify all intents that the bot uses
@@ -23,11 +23,13 @@ client.on('message', message =>{  // Discord.js v13 renamed 'message' event to '
                 const Embed = new MessageEmbed()
                 .setTitle('Server Status')
                 .addFields(
-                    {name: 'Server IP',      value: "> " + config.server_ip},      // Discord.js v13 requires manual call of toString on all methods
+                    {name: 'Server IP',      value: "> " + config.server_ip},               // Discord.js v13 requires manual call of toString on all methods
+                    {name: 'Modpack',        value: "> " + response.motd.clean.toString()},
                     {name: 'Version',        value: "> " + response.version.name.toString()},
                     {name: 'Online Players', value: "> " + response.players.online.toString()},
                     )
                 .setColor("#8570C1")
+                .setFooter('Server Online')
 
                 message.channel.send(Embed);    // v13: send({embeds: [Embed]})
             })
