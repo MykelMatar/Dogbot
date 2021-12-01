a3ec6b0ee0bc0dfc6dd4aac77be12873372c0eb6
mykelmatar <Michaelfm01@gmail.com>
2021-11-30 16:25:42 -0800

    Updated to discord.js v13
    issue: minecraft server util returns "null" instead of server IP, not sure why.

--- index.js
+++ index.js
@@ -1,46 +1,43 @@
-const {Client, MessageEmbed, Message} = require('discord.js');
+const {Client, Intents, MessageEmbed, Message} = require('discord.js');
 const config = require('./config.json');
-const ping = require('minecraft-server-util')
-const client = new Client();
+const util = require('minecraft-server-util')
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });   // Discord.js 13 requires user to specify all intents that the bot uses
 const PREFIX = '!'
-
 
 
 client.once('ready', () => {
     console.log('ready');
 });
 
-client.on('message', message =>{
+client.on('messageCreate', message =>{  // Discord.js v13 renamed 'message' event to 'messageCreate'
 
     let args = message.content.substring(PREFIX.length).split(' ')
 
     switch (args[0]){
         case 'mc' :
-
-
-            ping.status(config.server_ip) // port is default 25565
-                .then((response) => {
+        util.status(config.server_ip) // port default is 25565
+            .then((response) => {
                 console.log(response);
-                const Embed = new MessageEmbed();
-                Embed.setTitle('Server Status');
-                Embed.addField('Server IP', response.host);
-                Embed.addField('Server Version',response.version);
-                Embed.addField('Online Players', response.onlinePlayers);
 
+                // create embed
+                const Embed = new MessageEmbed()
+                .setTitle('Server Status')
+                .addFields(
+                    {name: 'Server IP',      value: "> " + config.server_ip},      // Discord.js v13 requires manual call of toString on all methods
+                    {name: 'Version',        value: "> " + response.version.name.toString()},
+                    {name: 'Online Players', value: "> " + response.players.online.toString()},
+                    )
+                .setColor("#8570C1")
 
-                message.channel.send(Embed);
-
-                
+                message.channel.send({embeds: [Embed]});
             })
-                 .catch((error) => {
+            .catch((error) => {
                 console.error(error);
-                message.channel.send('Server Offline');
+                message.channel.send({content: 'Server Offline'});
             });
-            
         break;
     }
 })
-
 
 client.login(config.bot_token);
 
