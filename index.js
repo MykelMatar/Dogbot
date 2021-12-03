de3b1008bae0f17869fc59d48ea108aa0123a748
mykelmatar <Michaelfm01@gmail.com>
2021-12-03 12:51:20 -0800

    Update index.js

--- index.js
+++ index.js
@@ -15,42 +15,19 @@ client.on('message', message =>{  // Discord.js v13 renamed 'message' event to '
     let tmpStatus = 0;
     let status = 0;
 
-
-    function serverOfflineReponse(){
-        const Embed = new MessageEmbed()
-            .setTitle("Dogbert's Server 2.0")
-            .addField("Server Offline", "all good")
-            .setColor("#8570C1")
-
-        message.channel.send(Embed);    // v13: send({embeds: [Embed]})
-    }
-
-    function serverOnlineResponse(){
-        const Embed = new MessageEmbed()
-            .setTitle("Dogbert's Server 2.0")
-            .addFields(
-                {name: 'Server IP',      value: "> " + response.host},               // Discord.js v13 requires manual call of toString on all methods
-                {name: 'Modpack',        value: "> " + response.description.toString()},
-                {name: 'Version',        value: "> " + response.version.toString()},
-                {name: 'Online Players', value: "> " + response.onlinePlayers.toString()},
-                )
-            .setColor("#8570C1")
-            .setFooter('Server Online')
-
-        message.channel.send(Embed);    // v13: send({embeds: [Embed]})
-    }
+    // helper functions
 
     function refreshStatus(){
         util.status(config.server_ip)
         console.log(response);
         if(tmpStatus != status){
-            if(tmpStatus == 1 && status == 1){ 
-                const recievedEmbed = await (await message.channel.messages.fetch(embedID)).embeds[0];
+            if(tmpStatus == 1 && status == 1){ // if server is online
+                const recievedEmbed = await ( message.channel.messages.fetch(embedID)).embeds[0];
                 const newEmbed = new MessageEmbed(recievedEmbed) //creates new embed to edit existing embed
                 newEmbed.Fields[1] = {name: 'Online Players', value: "> " + response.onlinePlayers.toString()}
             }
             else if (tmpStatus == 0 && status == 1){ // if server goes online
-                const recievedEmbed = await (await message.channel.messages.fetch(embedID)).embeds[0];
+                const recievedEmbed = await ( message.channel.messages.fetch(embedID)).embeds[0];
                 const newEmbed = new MessageEmbed(recievedEmbed) //creates new embed to edit existing embed
                 newEmbed.Fields[1] = {name: 'Server IP',      value: "> " + response.host}
                 newEmbed.Fields[1] = {name: 'Modpack',        value: "> " + response.description.toString()}
@@ -59,7 +36,7 @@ client.on('message', message =>{  // Discord.js v13 renamed 'message' event to '
                 newEmbed.Footer = "Server Online"
             }
             else if(tmpStatus == 1 && status == 0){ // if server goes offline
-                const recievedEmbed = await (await message.channel.messages.fetch(embedID)).embeds[0];
+                const recievedEmbed = await ( message.channel.messages.fetch(embedID)).embeds[0];
                 const newEmbed = new MessageEmbed(recievedEmbed) 
                 newEmbed.Fields[1] = {name: "Server Offline", value: "all good"};
             }
@@ -69,28 +46,46 @@ client.on('message', message =>{  // Discord.js v13 renamed 'message' event to '
 
     const intervalObj = setInterval((refreshStatus) => {
         console.log("refreshed");
-    }, 10000);     // 300000ms = 5m
+    }, 20000);     // 300000ms = 5m
 
+
+    // command
     if(message.content == "!mc"){
         util.status(config.server_ip) // port default is 25565
             .then((response) => {
                 console.log(response)
                 status = 1;
-                serverOnlineResponse();
+                const Embed = new MessageEmbed()
+                .setTitle("Dogbert's Server 2.0")
+                .addFields(
+                    {name: 'Server IP',      value: "> " + response.host},               // Discord.js v13 requires manual call of toString on all methods
+                    {name: 'Modpack',        value: "> " + response.description.toString()},
+                    {name: 'Version',        value: "> " + response.version.toString()},
+                    {name: 'Online Players', value: "> " + response.onlinePlayers.toString()},
+                    )
+                .setColor("#8570C1")
+                .setFooter('Server Online')
+
+            message.channel.send(Embed);    // v13: send({embeds: [Embed]})
             })
             .catch((error) => {
                 console.error(error)
                 status = 0;
-                serverOfflineReponse();
+                const Embed = new MessageEmbed()
+                .setTitle("Dogbert's Server 2.0")
+                .addField("Server Offline", "all good")
+                .setColor("#8570C1")
+    
+                message.channel.send(Embed);    // v13: send({embeds: [Embed]})
             });
-            intervalObj();
+            intervalObj;
     }
 
-    if (message.content === "!mc" || "!stop") {
-        message.delete(); 
-    }
+    // if (message.content === "!mc" || "!stop") {
+    //     message.delete(); 
+    // }
 
-    if(message.content == "stop"){
+    if(message.content === "clr"){
         clearTimeout(intervalObj);
     }
 })
