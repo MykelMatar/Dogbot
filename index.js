9ab2b41806d21dfde46b0845afb51ea0f69935ee
mykelmatar <Michaelfm01@gmail.com>
2021-12-09 02:06:10 -0800

    updated to discord.js 13

--- index.js
+++ index.js
@@ -1,38 +1,38 @@
-const {Client, MessageEmbed, Message} = require('discord.js');
+const { Client, MessageEmbed, Message } = require('discord.js');
+const client = new Client({ intents: ['GUILDS', 'GUILD_MESSAGES'] });   // Discord.js 13 requires user to specify all intents that the bot uses
 const util = require('minecraft-server-util');
 const config = require('./config.json');
 const data = require("./data.json");
 const { clearInterval } = require('timers');
-const client = new Client(); //({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });   // Discord.js 13 requires user to specify all intents that the bot uses
 const fs = require("fs");
 const PREFIX = '!'
 let tmpStatus = 0;
 let status = 0;
 
 client.once('ready', () => {
-    console.log('ready');
+  console.log('ready');
 });
 
 
-client.on("guildCreate", async function(guild) {
-    if (!("Guilds" in data)) {
-      var gData = {
-          Guilds: {},
-      };
-      writeToJson(gData);
-    }
-  
-    let guildName = guild.name.replace(/\s+/g, ""); //removes whitespace from string
-    let newJson = {
-      ServerData: {
-        serverId: guild.id,
-      },
-      EmbedData: {
-        id: "1"
-      },
+client.on("guildCreate", async function (guild) {
+  if (!("Guilds" in data)) {
+    var gData = {
+      Guilds: {},
     };
-    data.Guilds[guildName] = newJson;
-    writeToJson(data);
+    writeToJson(gData);
+  }
+
+  let guildName = guild.name.replace(/\s+/g, ""); //removes whitespace from string
+  let newJson = {
+    ServerData: {
+      serverId: guild.id,
+    },
+    EmbedData: {
+      id: "1"
+    },
+  };
+  data.Guilds[guildName] = newJson;
+  writeToJson(data);
 });
 
 
@@ -41,110 +41,110 @@ client.login(config.bot_token);
 
 
 
-client.on("message", async (message) =>{  // Discord.js v13 renamed 'message' event to 'messageCreate'
+client.on("messageCreate", async (message) => {  // Discord.js v13 renamed 'message' event to 'messageCreate'
 
-    let guildname = message.guild.name.replace(/\s+/g, "");
-    let embedID = data.Guilds[guildname].EmbedData["id"];
-    let args = message.content.substring(PREFIX.length).split(' ')
+  let guildname = message.guild.name.replace(/\s+/g, "");
+  let embedID = data.Guilds[guildname].EmbedData["id"];
+  let args = message.content.substring(PREFIX.length).split(' ')
 
-    // command
-    if(message.content == "!mc"){
-        clearInterval(refresh);       // clear existing refreshes
-        util.status(config.server_ip) // port default is 25565
-            .then((response) => {
-              console.log(response)
-              status = 1;
-              tmpStatus = 1;
+  // command
+  if (message.content == "!mc") {
+    clearInterval(refresh);       // clear existing refreshes
+    util.status(config.server_ip) // port default is 25565
+      .then((response) => {
+        console.log(response)
+        status = 1;
+        tmpStatus = 1;
 
-              const Embed = new MessageEmbed()
-                .setTitle("Dogbert's Server 2.0")
-                .addFields(
-                    {name: 'Server IP',      value: "> " + response.host.toString()},               // Discord.js v13 requires manual call of toString on all methods
-                    {name: 'Modpack',        value: "> " + response.description.toString()},
-                    {name: 'Version',        value: "> " + response.version.toString()},
-                    {name: 'Online Players', value: "> " + response.onlinePlayers.toString()},
-                    )
-                .setColor("#8570C1")
-                .setFooter('Server Online')
+        const Embed = new MessageEmbed()
+          .setTitle("Dogbert's Server 2.0")
+          .addFields(
+            { name: 'Server IP', value: "> " + config.server_ip.toString() },               // Discord.js v13 requires manual call of toString on all methods
+            { name: 'Modpack', value: "> " + response.motd.clean.toString() },
+            { name: 'Version', value: "> " + response.version.name.toString() },
+            { name: 'Online Players', value: "> " + response.players.online.toString() },
+          )
+          .setColor("#8570C1")
+          .setFooter('Server Online')
 
-              message.channel.send(Embed); 
-            })
-            .catch((error) => {
-              console.error(error)
-              status = 0;
+        message.channel.send({ embeds: [Embed] });
+      })
+      .catch((error) => {
+        console.error(error)
+        status = 0;
 
-              const Embed = new MessageEmbed()
-                .setTitle("Dogbert's Server 2.0")
-                .addField("Server Offline", "all good")
-                .setColor("#8570C1")
-              Embed.fields[1] = [];
-              Embed.fields[2] = [];
-              Embed.fields[3] = [];
-              Embed.fields[4] = [];
-              Embed.setFooter([]);
+        const Embed = new MessageEmbed()
+          .setTitle("Dogbert's Server 2.0")
+          .addField("Server Offline", "all good")
+          .setColor("#8570C1");
+        Embed.fields[1] = []
+        Embed.fields[2] = []
+        Embed.fields[3] = []
+        Embed.fields[4] = [];
+        Embed.setFooter('');
 
-              message.channel.send(Embed);    // v13: send({embeds: [Embed]})
-            });  
-    }
+        message.channel.send({ embeds: [Embed] });    // v13: send({embeds: [Embed]})
+      });
+  }
 
-    if (message.embeds[0]) {
-      //push embed id to json
-      data.Guilds[guildname].EmbedData["id"] = message.id;
-      writeToJson(data);
-      var refresh = setInterval(refreshStatus, 300000 , message, guildname); // 300000
+  if (message.embeds[0]) {
+    //push embed id to json
+    data.Guilds[guildname].EmbedData["id"] = message.id;
+    writeToJson(data);
+    var refresh = setInterval(refreshStatus, 10000, message, guildname); // 300000
   }
 });
 
 
 
 
-async function refreshStatus(message, guildname){
-  if (message.author.bot) {
+async function refreshStatus(messageCreate, guildname) {
+  if (messageCreate.author.bot) {
     let embedID = data.Guilds[guildname].EmbedData["id"];
 
     util.status(config.server_ip)
       .then(async response => {
         status = 1
-        if(tmpStatus == 1 && status == 1){ // if server status hasnt changed, update player count
-          const recievedEmbed =  await (await message.channel.messages.fetch(embedID)).embeds[0];
+        if (tmpStatus == 1 && status == 1) { // if server status hasnt changed, update player count
+          const recievedEmbed = await (await messageCreate.channel.messages.fetch(embedID)).embeds[0];
           const newEmbed = new MessageEmbed(recievedEmbed) //creates new embed to edit existing embed
-          newEmbed.fields[3] = {name: 'Online Players', value: "> " + response.onlinePlayers.toString()};
+          newEmbed.fields[3] = { name: 'Online Players', value: "> " + response.players.online.toString() };
 
-          message.edit(newEmbed);
+          messageCreate.edit({ embeds: [newEmbed] });
           console.log('refreshed player count')
-      }
-      if(tmpStatus != status) // if server status changed, update embed
-        if (tmpStatus == 0 && status == 1){ // if server goes online
+        }
+        if (tmpStatus != status) // if server status changed, update embed
+          if (tmpStatus == 0 && status == 1) { // if server goes online
 
-          const recievedEmbed =  await (await message.channel.messages.fetch(embedID)).embeds[0];
-          const newEmbed = new MessageEmbed(recievedEmbed) //creates new embed to edit existing embed
-          newEmbed.fields[0] = []
-          newEmbed.fields[1] = {name: 'Server IP',      value: "> " + response.host}
-          newEmbed.fields[2] = {name: 'Modpack',        value: "> " + response.description.toString()}
-          newEmbed.fields[3] = {name: 'Version',        value: "> " + response.version.toString()}
-          newEmbed.fields[4] = {name: 'Online Players', value: "> " + response.onlinePlayers.toString()}
-          newEmbed.setFooter("Server Online");
+            const recievedEmbed = await (await messageCreate.channel.messages.fetch(embedID)).embeds[0];
+            const newEmbed = new MessageEmbed(recievedEmbed) //creates new embed to edit existing embed
+            newEmbed.fields[0] = []
+            newEmbed.fields[1] = { name: 'Server IP', value: "> " + config.server_ip.toString() }
+            newEmbed.fields[2] = { name: 'Modpack', value: "> " + response.motd.clean.toString() }
+            newEmbed.fields[3] = { name: 'Version', value: "> " + response.version.name.toString() }
+            newEmbed.fields[4] = { name: 'Online Players', value: "> " + response.players.online.toString() }
+            newEmbed.setFooter("Server Online");
 
-          message.edit(newEmbed);
-          console.log('refreshed server status')
-        } 
+            messageCreate.edit({ embeds: [newEmbed] });
+            console.log('refreshed server status')
+          }
       })
       .catch(async (error) => {
         console.error(error)
         status = 0;
 
-        const recievedEmbed = await (await message.channel.messages.fetch(embedID)).embeds[0];
-        const newEmbed = new MessageEmbed(recievedEmbed) 
-        newEmbed.fields[0] = {name: "Server Offline", value: "all good"};
+        const recievedEmbed = await (await messageCreate.channel.messages.fetch(embedID)).embeds[0];
+        const newEmbed = new MessageEmbed(recievedEmbed)
+        newEmbed.fields[0] = { name: "Server Offline", value: "all good" };
         newEmbed.fields[1] = [];
         newEmbed.fields[2] = [];
         newEmbed.fields[3] = [];
         newEmbed.fields[4] = [];
-        newEmbed.setFooter([]);
+        newEmbed.setFooter('');
 
-        message.edit(newEmbed);
+        messageCreate.edit({ embeds: [newEmbed] });
         console.log('refreshed server status')
-    });
+      });
     tmpStatus = status;
   }
 }
@@ -153,7 +153,7 @@ async function refreshStatus(message, guildname){
 
 //writes to data.json
 function writeToJson(data) {
-  fs.writeFile("./data.json", JSON.stringify(data, null, 4), function(err) {
+  fs.writeFile("./data.json", JSON.stringify(data, null, 4), function (err) {
     if (err) throw err;
   });
 }
