6853a0111cbd97620deb8e12c31a4277b3efdf9a
mykelmatar <Michaelfm01@gmail.com>
2021-12-26 00:08:53 -0800

    created function to prevent collector collisions

---
+++ helperFunctions/preventInteractionCollision.js
@@ -0,0 +1,15 @@
+
+async function preventInteractionCollision(message, collector, sent) {
+    cmdList = ['!renamemc', '!delmc', '!changemc']  // commands who's collectors collide with each other if executed simultaneously 
+
+    const msgCollector = message.channel.createMessageCollector({ time: 15000 })
+    return msgCollector.on('collect', async m => {
+        if (cmdList.includes(m.content)) { 
+            msgCollector.stop();
+            collector.stop();
+            await sent.edit({ content: 'command aborted', ephemeral: true, components: [] })
+        }
+    });
+}
+
+module.exports = preventInteractionCollision;
