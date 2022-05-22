d5c8c936f235237ad45e6a50de22c4e4fe518c46
mykelmatar <Michaelfm01@gmail.com>
2022-05-22 13:08:07 -0700

    deleted everything

--- commands/help/simleave.js
+++
@@ -1,8 +0,0 @@
-
-module.exports = {
-    name: 'simleave', 
-    description: 'simulates a leave for testing', 
-    async execute(client, message){
-        client.emit('guildMemberRemove', message.member)
-    }
-}
