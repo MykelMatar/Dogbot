6e3108aa29c28605e78560e0611d889537cb3d23
Ismail Hasan <ismailhasan0132@gmail.com>
2023-01-30 22:44:40 -0800

    bot now automatically joins voice channel with first user who joins a voice channel

---
+++ src/events/client/voiceStateUpdate.ts
@@ -0,0 +1,36 @@
+import log from "../../dependencies/logger";
+import {NewClient} from "../../dependencies/myTypes";
+import { VoiceState } from "discord.js";
+import {getVoiceConnection, joinVoiceChannel} from "@discordjs/voice";
+
+export async function voiceStateUpdate(client: NewClient, oldState: VoiceState, newState: VoiceState) {
+    // Check if the bot is joining a voice channel
+    if (!oldState.channelId && newState.channelId) {
+        // Check if bot is already in a voice channel
+        const connection = getVoiceConnection(newState.guild.id);
+        if(connection) return;
+        // Check if the user joining the voice channel is the first user
+        if (newState.channel.members.size >= 1) {
+            // Join the same voice channel as the first user
+            joinVoiceChannel({
+                channelId: newState.channelId,
+                guildId: newState.guild.id,
+                selfDeaf: false,
+                // @ts-ignore (possible issue with library. Thinks these 2 are incompatible)
+                adapterCreator: newState.guild.voiceAdapterCreator,
+            });
+        }
+    }
+
+    // Check if the bot is leaving a voice channel
+    if (oldState.channelId && !newState.channelId || oldState.channelId && newState.channelId) {
+        // Check if the bot is the only user left in the voice channel
+        if (oldState.channel.members.size === 1) {
+            // Leave the voice channel
+            const connection = getVoiceConnection(oldState.guild.id);
+            connection.destroy()
+        }
+    }
+
+    log.info(`${oldState.channelId} && ${newState.channelId}`);
+}
