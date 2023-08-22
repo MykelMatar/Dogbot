440f8c5369862d9a8d1b6cbef0abf26661a1189e
mykelmatar <Michaelfm01@gmail.com>
2023-08-21 22:31:08 -0500

    trying to fix fetch gamers timer

--- src/dependencies/helpers/fetchHelpers/calculateFetchTimer.ts
+++ src/dependencies/helpers/fetchHelpers/calculateFetchTimer.ts
@@ -21,6 +21,7 @@ export default function(client, time, guildData: MongoGuild): number {
     if (validTime) {
         const now = DateTime.local();
         const centralTime = now.setZone('America/Chicago')
+        console.log({centralTime})
         // daylight savings check
         let offset = guildData.settings.timeZone.offset
         const daylightSavingsZones = ['-10', '-9', '-8', '-7', '-6', '-5', '-4']
@@ -30,10 +31,10 @@ export default function(client, time, guildData: MongoGuild): number {
             offset = newOffset.toString()
         }
 
-        validTime = validTime.setZone(`UTC${offset}`, {keepLocalTime: true})
         if (!client.isTestBot) { // not sure why but server is 1 day ahead??
             validTime.minus({days: 1})
         }
+        validTime = validTime.setZone(`UTC${offset}`, {keepLocalTime: true})
         const nowRezoned = now.setZone(`UTC${offset}`)
         console.log(`UTC${offset}`, validTime, nowRezoned)
         // If the future time is earlier than the current time, add one day to the future time
